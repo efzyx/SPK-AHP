af333680e60e0ffbc12043ad1af7197866c39a22
Muhammad Fauzi <fauzi.padlaw@gmail.com>
2017-03-28 19:22:45 +0700

    tambah perbandingan criterion

--- app/controllers/criterion_comparisons_controller.rb
+++ app/controllers/criterion_comparisons_controller.rb
@@ -1,10 +1,11 @@
 class CriterionComparisonsController < ApplicationController
-  cri = Criterion.all unless Criterion.nil?
-  ccs = CriterionComparison.all
+  $cri = Criterion.all unless Criterion.nil?
+  $ccs = CriterionComparison.all.order(criterion_id: :asc)
   def index
     @cc = CriterionComparison.new
-    @ccs = ccs
-    @criterions = cri
+    @ccs = $ccs
+    @criterions = $cri
+    @curretCri = get_comparison_criterion
   end
 
   def create
@@ -12,7 +13,7 @@ class CriterionComparisonsController < ApplicationController
     if @cc.save
       redirect_to criterion_comparisons_path, notice: 'Berhasil menambahkan perbandingan'
     else
-      render criterion_comparisons_path
+      redirect_to criterion_comparisons_path
     end
   end
 
@@ -22,13 +23,39 @@ class CriterionComparisonsController < ApplicationController
   end
 
   def get_comparison_criterion
-    @sumCri = cri.count
+    @sumCri = $cri.count
     @curretCri1 = nil
     @curretCri2 = nil
 
-    if ccs.nil?
-      @curretCri1 = cri.first.id
-      @curretCri2 = @curretCri1.next
-
+    if $ccs.empty?
+      @curretCri1 = $cri.first.id
+      @curretCri2 = $cri.first.next.id
+    else
+      @ketemu = false
+      $cri.each do |c|
+        if $ccs.where(criterion_id: c.id).exists?
+          @cri1 = $ccs.where(criterion_id: c.id)
+          @cri2 = $ccs.where(other_criterion_id: c.id)
+          @latestCri = @cri1.last
+          @latestCri2 = @latestCri.other_criterion_id
+          @criForCurrentCri2 = $cri.find(@latestCri2).next
+          if (@cri1.count + @cri2.count) < @sumCri-1
+            @curretCri1 = c.id
+            @curretCri2 = @criForCurrentCri2.id
+            @ketemu = true
+          end
+        elsif
+          begin
+          @curretCri1 = c.id
+          @curretCri2 = c.next.id
+          @ketemu = true
+          rescue
+            @ketemu = true
+          end
+        end
+        break if @ketemu == true
+      end
+    end
+    return [@curretCri1, @curretCri2]
   end
 end
