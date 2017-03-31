f7a63c6af4f8c2e38255d951a893cdcec90458a9
Muhammad Fauzi <fauzi.padlaw@gmail.com>
2017-03-31 13:24:40 +0700

    added consistency

--- app/controllers/criterion_comparisons_controller.rb
+++ app/controllers/criterion_comparisons_controller.rb
@@ -1,4 +1,5 @@
 class CriterionComparisonsController < ApplicationController
+  require 'matrix'
   before_action :authenticate_user! , except: [:index]
   before_action :set_cc, only: [:edit, :update, :destroy]
   def index
@@ -117,12 +118,69 @@ class CriterionComparisonsController < ApplicationController
     end
     return @average
   end
+
+  def self.get_matrix_cri
+    @data = self.get_comparison_table
+    return Matrix.columns(@data)
+  end
   private
   def cc_params
     params.require(:criterion_comparison).permit(:criterion_id, :comparison, :other_criterion_id)
   end
 
   def get_comparison_criterion
+    @cri = Criterion.all.order(id: :asc) unless Criterion.nil?
+    @ccs = CriterionComparison.all.order(criterion_id: :asc, other_criterion_id: :asc)
+    @sumCri = @cri.count
+    @curretCri1 = nil
+    @curretCri2 = nil
+
+    if @ccs.empty?
+      @curretCri1 = @cri.first.id
+      @curretCri2 = @cri.first.next.id
+    else
+      @ketemu = false
+      @cri.each do |c|
+        if @ccs.where(criterion_id: c.id).exists?
+          @cri1 = @ccs.where(criterion_id: c.id)
+          @cri2 = @ccs.where(other_criterion_id: c.id)
+
+          @kriteria = c.next
+          @cri1.each do |cr|
+            @stop = false
+            @cri1.each do |cr2|
+              if cr2.other_criterion_id == @kriteria.id
+                @stop = true
+              end
+              break if @stop == true
+            end
+            break if @stop == false
+            @kriteria = @kriteria.next
+          end
+
+
+          if (@cri1.count + @cri2.count) < @sumCri-1
+            @curretCri1 = c.id
+            @curretCri2 = @kriteria.id
+            @ketemu = true
+          end
+
+        else
+          begin
+            @curretCri1 = c.id
+            @curretCri2 = c.next.id
+            @ketemu = true
+          rescue
+            @ketemu = true
+          end
+        end
+        break if @ketemu == true
+      end
+    end
+    return [@curretCri1, @curretCri2]
+  end
+
+  def self.get_comparison_criterion
     @cri = Criterion.all.order(id: :asc) unless Criterion.nil?
     @ccs = CriterionComparison.all.order(criterion_id: :asc, other_criterion_id: :asc)
     @sumCri = @cri.count
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -17,9 +17,28 @@ class WelcomeController < ApplicationController
           @reschart[ind] = [@altNow.name, r]
           @altNow = @altNow.next
         end
+        @mCri = getMatrixCriFromCCC
+        @matrixTAve = @mCri * @matriksCri
+        @arrayTAve = @matrixTAve.to_a
+
+        @sumT = 0
+        @arraySumT=[]
+        @arrayTAve.each_with_index do |t, ind|
+          @sumT += t[0] / @aveCri[ind]
+          @arraySumT[ind] = t[0] / @aveCri[ind]
+        end
+        @aTA = @arrayTAve.count.to_f
+        @aveT = @sumT.to_f/@aTA
+        @consIndex = (@aveT-@aTA)/(@aTA-1.to_f)
+        @random = [0,0,0.58,0.9, 1.12, 1.24, 1.32, 1.41, 1.45, 1.49, 1.51, 1.48, 1.56, 1.57, 1.59]
+        begin
+          @consist = @consIndex / @random[@aTA-1]
+        rescue
+          @consist = @consIndex / 0
+        end
+
       rescue
-        @dataAve = nil
-        flash[:warning] = 'Error! Periksa kelengkapan data'
+        @dataAve = []
       end
   end
 
@@ -29,5 +48,9 @@ class WelcomeController < ApplicationController
 
   def getAverageCri
     return CriterionComparisonsController.get_criterion_average
+  end
+
+  def getMatrixCriFromCCC
+    return CriterionComparisonsController.get_matrix_cri
   end
 end
